867e606fcb3b4eb282ec4fe99c0a4098224838a7
khsoh <2169449+khsoh@users.noreply.github.com>
2024-02-07 15:32:53 +0800

    Tidied up the config

--- init.lua
+++ init.lua
@@ -199,20 +199,21 @@ require('lazy').setup({
       end,
     },
   },
-
+--[[
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
+    priority = 1000,
     lazy = false,
     config = function()
       require('onedark').setup {
         -- Set a style preset. 'dark' is default.
         style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
       }
-      -- require('onedark').load()
+      require('onedark').load()
     end,
   },
-
+]]
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -11,13 +11,15 @@ return {
     "catppuccin/nvim",
     priority = 1200,
     lazy = false,
-    flavour = "mocha",  -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-      light = "latte",
-      dark = "mocha",
-    },
     config = function()
-      vim.cmd.colorscheme('catppuccin')
+      require('catppuccin').setup {
+        flavour = "mocha",  -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+      }
+      require('catppuccin').load()
     end,
   },
 }
