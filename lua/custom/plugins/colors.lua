7e6ed9ce4dae464938727f4b618d69939893babd
khsoh <2169449+khsoh@users.noreply.github.com>
2024-02-07 13:17:08 +0800

    Move vim options and keymaps to custom plugins

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,8 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1100,
-  lazy = false,
-  config = function()
-    vim.cmd.colorscheme('tokyonight')
-  end,
+  {
+    "folke/tokyonight.nvim",
+    priority = 1100,
+    lazy = false,
+    config = function()
+      -- vim.cmd.colorscheme('tokyonight')
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    priority = 1200,
+    lazy = false,
+    flavour = "mocha",  -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+      light = "latte",
+      dark = "mocha",
+    },
+    config = function()
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
 }
