1f697f556d87f504aaa7ecc8ae3d49de3cc6ab9b
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-03-03 22:58:22 +0800

    Changes to reduce differences from upstream

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,22 +1,14 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    priority = 1100,
-    lazy = false,
-    config = function()
-      -- vim.cmd.colorscheme('tokyonight')
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    priority = 1200,
+    'catppuccin/nvim',
+    priority = 900,
     lazy = false,
     config = function()
       require('catppuccin').setup {
-        flavour = "mocha",  -- latte, frappe, macchiato, mocha
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
         background = { -- :h background
-          light = "latte",
-          dark = "mocha",
+          light = 'latte',
+          dark = 'mocha',
         },
       }
       require('catppuccin').load()
