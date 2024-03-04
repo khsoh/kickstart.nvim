4b341d08026b33ffb5385a92748b96c9b79ef5f0
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-03-04 15:06:10 +0800

    Added comment to disable warning for tsextend.lua

--- lua/custom/plugins/tsextend.lua
+++ lua/custom/plugins/tsextend.lua
@@ -2,6 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
       ensure_installed = { 'go', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'html' },
       auto_install = true,
