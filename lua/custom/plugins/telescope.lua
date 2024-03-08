92c10637e2fa60c8591e623fedfcce4f390c6a4e
K H Soh <hju37823@outlook.com>
2024-03-08 10:15:19 +0800

    Fixed telescope git key descriptions

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -4,12 +4,12 @@ return {
   keys = function()
     local builtin = require 'telescope.builtin'
     return {
-      { '<leader>gf', mode = { 'n' }, builtin.git_files, desc = '[Glit [f]iles - search files tracked by git' },
-      { '<leader>gb', mode = { 'n' }, builtin.git_branches, desc = '[Glit [b]ranches for current directory' },
-      { '<leader>gc', mode = { 'n' }, builtin.git_commits, desc = '[Glit [c]ommits for current directory' },
-      { '<leader>gt', mode = { 'n' }, builtin.git_commits, desc = '[Glit commi[t]s for this buffer' },
-      { '<leader>gs', mode = { 'n' }, builtin.git_status, desc = '[Glit [s]tatus for current directory' },
-      { '<leader>gh', mode = { 'n' }, builtin.git_stash, desc = '[Glit stas[h] items in currenct repo' },
+      { '<leader>gf', mode = { 'n' }, builtin.git_files, desc = '[G]it [f]iles - search files tracked by git' },
+      { '<leader>gb', mode = { 'n' }, builtin.git_branches, desc = '[G]it [b]ranches for current directory' },
+      { '<leader>gc', mode = { 'n' }, builtin.git_commits, desc = '[G]it [c]ommits for current directory' },
+      { '<leader>gt', mode = { 'n' }, builtin.git_commits, desc = '[G]it commi[t]s for this buffer' },
+      { '<leader>gs', mode = { 'n' }, builtin.git_status, desc = '[G]it [s]tatus for current directory' },
+      { '<leader>gh', mode = { 'n' }, builtin.git_stash, desc = '[G]it stas[h] items in currenct repo' },
     }
   end,
 }
