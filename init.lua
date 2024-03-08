da331844265e0b8b1ef13920bc3c253d7eddc8d4
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-03-08 15:36:34 +0800

    Tidy the treesitter setup

--- init.lua
+++ init.lua
@@ -791,7 +791,22 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = {
+          'go',
+          'python',
+          'rust',
+          'tsx',
+          'javascript',
+          'typescript',
+          'html',
+          'bash',
+          'c',
+          'html',
+          'lua',
+          'markdown',
+          'vim',
+          'vimdoc',
+        },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
