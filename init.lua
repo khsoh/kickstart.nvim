ad0153cc8b70cefa86b34e0f2b5f825245f4ed17
K H Soh <hju37823@outlook.com>
2024-09-10 15:26:36 +0800

    Renamed tsserver to ts_ls

--- init.lua
+++ init.lua
@@ -614,8 +614,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {},
         html = { filetypes = { 'html', 'twig', 'hbs' } },
         marksman = {},
         powershell_es = {
