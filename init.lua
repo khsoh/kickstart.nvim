d6e3ffe1c5b11e42dc677a6fc6fc34cc187696fd
K H Soh <hju37823@outlook.com>
2026-02-02 11:19:21 +0800

    Added bash language server

--- init.lua
+++ init.lua
@@ -597,6 +597,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         ['rust-analyzer'] = {},
+        ['bash-language-server'] = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
