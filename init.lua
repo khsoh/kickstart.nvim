add299d07579e5b110fbb06968f1c0553cfa5cf3
K H Soh <hju37823@outlook.com>
2026-05-22 09:53:13 +0800

    Replaced colorscheme with minisummer

--- init.lua
+++ init.lua
@@ -391,30 +391,20 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
-  }
+  -- OVERRIDE
+  -- vim.pack.add { gh 'folke/tokyonight.nvim' }
+  -- ---@diagnostic disable-next-line: missing-fields
+  -- require('tokyonight').setup {
+  --   styles = {
+  --     comments = { italic = false }, -- Disable italics in comments
+  --   },
+  -- }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
   -- vim.cmd.colorscheme 'tokyonight-night' -- OVERRIDE
-
-  -- OVERRIDE
-  vim.pack.add { gh 'catppuccin/nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('catppuccin').setup {
-    flavour = 'mocha', -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-      light = 'latte',
-      dark = 'mocha',
-    },
-  }
-  vim.cmd.colorscheme 'catppuccin'
+  vim.cmd.colorscheme 'minisummer'
   -- END OVERRIDE
 
   -- Highlight todo, notes, etc in comments
